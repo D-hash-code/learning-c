0f64985e00dbe91172eab1021fe07fe8bddcb421
D-hash-code <73314268+D-hash-code@users.noreply.github.com>
2022-02-15 19:43:25 +0000

    replacing lower, upper and step with define consts?vars?

--- ch1TutorialIntro/fahrcellfor.c
+++ ch1TutorialIntro/fahrcellfor.c
@@ -4,11 +4,15 @@
 
 #include <stdio.h>
 
+#define LOWER 0 /* the lower bound of the range of interest */
+#define UPPER 300 /* the upper bound of the range of interest */
+#define STEP 20 /* the size of the intervals we consider */
+
 int main()
 {
     int fahr;
 
-    for (fahr = 0; fahr <= 300; fahr = fahr + 20)
+    for (fahr = LOWER; fahr <= UPPER; fahr = fahr + STEP)
         printf("%3d %6.1f\n", fahr, (5.0/9.0)*(fahr-32));
 
     /* Exercise 1-5. Modify the program to print in reverse order */
