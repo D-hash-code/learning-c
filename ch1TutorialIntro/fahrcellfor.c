8e643d81d7e28f7513990a20cb8aae73b482ffd7
D-hash-code <73314268+D-hash-code@users.noreply.github.com>
2022-02-15 19:30:50 +0000

    exercise 1.5 kinda

--- ch1TutorialIntro/fahrcellfor.c
+++ ch1TutorialIntro/fahrcellfor.c
@@ -10,4 +10,11 @@ int main()
 
     for (fahr = 0; fahr <= 300; fahr = fahr + 20)
         printf("%3d %6.1f\n", fahr, (5.0/9.0)*(fahr-32));
+
+    /* Exercise 1-5. Modify the program to print in reverse order */
+
+    while (fahr >= 0){
+        printf("%3d %6.1f\n", fahr, (5.0/9.0)*(fahr-32));
+        fahr = fahr - 20;
+    }
 }
